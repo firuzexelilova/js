4445a84573f62cd7448fb18385444209f2d00fd7
firuzexelilova <76410570+firuzexelilova@users.noreply.github.com>
2021-02-11 05:54:17 +0400

    Add files via upload

---
+++ JS/js/app.js
@@ -0,0 +1,13 @@
+function Exchange(){
+    var deyer = document.getElementById('inp').value;
+var text = document.getElementById('text');
+    var manat = deyer * 1.7;
+
+    if(deyer !=""){
+        alert(deyer + "dollar" + manat + "manata beraberdir");   
+     }
+
+     else{
+
+     }
+}
